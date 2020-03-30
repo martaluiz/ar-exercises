3af9e2e01f727479dda438d318b17addeac111f7
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-03-30 22:35:15 +0000

    select sum, avg highest revenue stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: #{@total_revenue}"
+
+@average_avenue = Store.average(:annual_revenue)
+puts "Average Avenue : #{@average_avenue}"
+
+
+@highest_revenue_stores = Store.where("annual_revenue > ?", 1000000).count
+puts "#{@highest_revenue_stores}"
