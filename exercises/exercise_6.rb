c2035da3ce967ebe8286786d0bd402300c509c1e
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-03-30 23:55:31 +0000

    display errors

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Maria", last_name: "Wings", hourly_rate: 60)
+@store1.employees.create(first_name: "Erick", last_name: "gnow", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Kaio", last_name: "Snow", hourly_rate: 90)
+@store2.employees.create(first_name: "Derick", last_name: "Doors", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "What is the name of your store?"
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+puts "Is 'new_store' Persisted? #{new_store.persisted?}"
+
+if new_store.errors.any?
+  puts "\nnew_store errors:"
+  puts new_store.errors.to_a
+else
+  puts "new_store names:"
+  puts new_store.name
+end
